52c52c52af1022d1bb78d53969e363402e7170d0
LuigiSalvatore <lu.mikich@gmail.com>
2024-04-14 15:13:26 -0300

    Added new struct for NPC

--- jogo.go
+++ jogo.go
@@ -1,101 +1,115 @@
 package main
 
 import (
-    "bufio"
-    "github.com/nsf/termbox-go"
-    "os"
-    "math/rand"
-    "time"
-    "sync"
-    "fmt"
+	"bufio"
+	"fmt"
+	"math/rand"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/nsf/termbox-go"
 )
 
 var mutex sync.Mutex
 
 // Define os elementos do jogo
 type Elemento struct {
-    simbolo rune
-    cor termbox.Attribute
-    corFundo termbox.Attribute
-    tangivel bool
+	simbolo  rune
+	cor      termbox.Attribute
+	corFundo termbox.Attribute
+	tangivel bool
 }
 
 // Personagem controlado pelo jogador
 var personagem = Elemento{
-    simbolo: '☺',
-    cor: termbox.ColorWhite,
-    corFundo: termbox.ColorDefault,
-    tangivel: true,
+	simbolo:  '☺',
+	cor:      termbox.ColorWhite,
+	corFundo: termbox.ColorDefault,
+	tangivel: true,
 }
 
 // Parede
 var parede = Elemento{
-    simbolo: '▣',
-    cor: termbox.ColorBlack|termbox.AttrBold|termbox.AttrDim,
-    corFundo: termbox.ColorDarkGray,
-    tangivel: true,
+	simbolo:  '▣',
+	cor:      termbox.ColorBlack | termbox.AttrBold | termbox.AttrDim,
+	corFundo: termbox.ColorDarkGray,
+	tangivel: true,
 }
 
 // Barrreira
 var barreira = Elemento{
-    simbolo: '#',
-    cor: termbox.ColorRed,
-    corFundo: termbox.ColorDefault,
-    tangivel: true,
+	simbolo:  '#',
+	cor:      termbox.ColorRed,
+	corFundo: termbox.ColorDefault,
+	tangivel: true,
 }
 
 // Vegetação
 var vegetacao = Elemento{
-    simbolo: '♣',
-    cor: termbox.ColorGreen,
-    corFundo: termbox.ColorDefault,
-    tangivel: false,
+	simbolo:  '♣',
+	cor:      termbox.ColorGreen,
+	corFundo: termbox.ColorDefault,
+	tangivel: false,
 }
 
 // Elemento vazio
 var vazio = Elemento{
-    simbolo: ' ',
-    cor: termbox.ColorDefault,
-    corFundo: termbox.ColorDefault,
-    tangivel: false,
+	simbolo:  ' ',
+	cor:      termbox.ColorDefault,
+	corFundo: termbox.ColorDefault,
+	tangivel: false,
 }
 
 // Elemento para representar áreas não reveladas (efeito de neblina)
 var neblina = Elemento{
-    simbolo: '.',
-    cor: termbox.ColorDefault,
-    corFundo: termbox.ColorYellow,
-    tangivel: false,
+	simbolo:  '.',
+	cor:      termbox.ColorDefault,
+	corFundo: termbox.ColorYellow,
+	tangivel: false,
 }
 
 var inimigo = Elemento{
-    simbolo: '☠',
-    cor: termbox.ColorRed,
-    corFundo: termbox.ColorDefault,
-    tangivel: true,
+	simbolo:  '☠',
+	cor:      termbox.ColorRed,
+	corFundo: termbox.ColorDefault,
+	tangivel: true,
 }
 
 var chave = Elemento{
-    simbolo: '⚷',
-    cor: termbox.ColorYellow,
-    corFundo: termbox.ColorDefault,
-    tangivel: false,
+	simbolo:  '⚷',
+	cor:      termbox.ColorYellow,
+	corFundo: termbox.ColorDefault,
+	tangivel: false,
 }
 
 var porta = Elemento{
-    simbolo: '⚑',
-    cor: termbox.ColorYellow,
-    corFundo: termbox.ColorBlack,
-    tangivel: false,
+	simbolo:  '⚑',
+	cor:      termbox.ColorYellow,
+	corFundo: termbox.ColorBlack,
+	tangivel: false,
+}
+
+var npc = Elemento{
+	simbolo:  'ツ',
+	cor:      termbox.ColorBlue,
+	corFundo: termbox.ColorDefault,
+	tangivel: true,
 }
 
 type Enemy struct {
-    x, y int
-    elem Elemento
-    alive bool
+	x, y  int
+	elem  Elemento
+	alive bool
+}
+type NonPlayerChar struct {
+	x, y       int
+	elem       Elemento
+	interacted bool
 }
 
 var i = Enemy{x: 0, y: 0, elem: inimigo, alive: true}
+var n = NonPlayerChar{x: 0, y: 0, elem: npc}
 
 var mapa [][]Elemento
 var posX, posY int
@@ -108,181 +122,182 @@ var revelado [][]bool
 var raioVisao int = 3
 
 func main() {
-    err := termbox.Init()
-    if err != nil {
-        panic(err)
-    }
-    defer termbox.Close()
+	err := termbox.Init()
+	if err != nil {
+		panic(err)
+	}
+	defer termbox.Close()
 
-    carregarMapa("mapa.txt")
-    if efeitoNeblina {
-        revelarArea()
-    }
-    desenhaTudo()
-    go logicaInimigo()
-    for !ganhei {
-        switch ev := termbox.PollEvent(); ev.Type {
-        case termbox.EventKey:
-            if ev.Key == termbox.KeyEsc {
-                return // Sair do programa
-            }
-            if ev.Ch == 'e' {
-                interagir()
-            } else {
-                mover(ev.Ch)
-                if efeitoNeblina {
-                    revelarArea()
-                }
-            }
-            desenhaTudo()
-        }
-    }
-    termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-    fmt.Println("Você ganhou!")
-    time.Sleep(2 * time.Second)
+	carregarMapa("mapa.txt")
+	if efeitoNeblina {
+		revelarArea()
+	}
+
+	desenhaTudo()
+	go logicaInimigo()
+	for !ganhei {
+		switch ev := termbox.PollEvent(); ev.Type {
+		case termbox.EventKey:
+			if ev.Key == termbox.KeyEsc {
+				return // Sair do programa
+			}
+			if ev.Ch == 'e' {
+				interagir()
+			} else {
+				mover(ev.Ch)
+				if efeitoNeblina {
+					revelarArea()
+				}
+			}
+			desenhaTudo()
+		}
+	}
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	fmt.Println("Você ganhou!")
+	time.Sleep(2 * time.Second)
 }
 
 func carregarMapa(nomeArquivo string) {
-    arquivo, err := os.Open(nomeArquivo)
-    if err != nil {
-        panic(err)
-    }
-    defer arquivo.Close()
+	arquivo, err := os.Open(nomeArquivo)
+	if err != nil {
+		panic(err)
+	}
+	defer arquivo.Close()
 
-    scanner := bufio.NewScanner(arquivo)
-    y := 0
-    for scanner.Scan() {
-        linhaTexto := scanner.Text()
-        var linhaElementos []Elemento
-        var linhaRevelada []bool
-        for x, char := range linhaTexto {
-            elementoAtual := vazio
-            switch char {
-            case parede.simbolo:
-                elementoAtual = parede
-            case barreira.simbolo:
-                elementoAtual = barreira
-            case vegetacao.simbolo:
-                elementoAtual = vegetacao
-            case personagem.simbolo:
-                // Atualiza a posição inicial do personagem
-                posX, posY = x, y
-                elementoAtual = vazio
-            case inimigo.simbolo:
-                i.x, i.y = x, y
-                elementoAtual = inimigo
-            case chave.simbolo:
-                elementoAtual = chave
-            case porta.simbolo:
-                elementoAtual = porta
-            }
-            linhaElementos = append(linhaElementos, elementoAtual)
-            linhaRevelada = append(linhaRevelada, false)
-        }
-        mapa = append(mapa, linhaElementos)
-        revelado = append(revelado, linhaRevelada)
-        y++
-    }
-    if err := scanner.Err(); err != nil {
-        panic(err)
-    }
+	scanner := bufio.NewScanner(arquivo)
+	y := 0
+	for scanner.Scan() {
+		linhaTexto := scanner.Text()
+		var linhaElementos []Elemento
+		var linhaRevelada []bool
+		for x, char := range linhaTexto {
+			elementoAtual := vazio
+			switch char {
+			case parede.simbolo:
+				elementoAtual = parede
+			case barreira.simbolo:
+				elementoAtual = barreira
+			case vegetacao.simbolo:
+				elementoAtual = vegetacao
+			case personagem.simbolo:
+				// Atualiza a posição inicial do personagem
+				posX, posY = x, y
+				elementoAtual = vazio
+			case inimigo.simbolo:
+				i.x, i.y = x, y
+				elementoAtual = inimigo
+			case chave.simbolo:
+				elementoAtual = chave
+			case porta.simbolo:
+				elementoAtual = porta
+			}
+			linhaElementos = append(linhaElementos, elementoAtual)
+			linhaRevelada = append(linhaRevelada, false)
+		}
+		mapa = append(mapa, linhaElementos)
+		revelado = append(revelado, linhaRevelada)
+		y++
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
 }
 
 func desenhaTudo() {
-    termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-    for y, linha := range mapa {
-        for x, elem := range linha {
-            if efeitoNeblina == false || revelado[y][x] {
-                termbox.SetCell(x, y, elem.simbolo, elem.cor, elem.corFundo)
-            } else {
-                termbox.SetCell(x, y, neblina.simbolo, neblina.cor, neblina.corFundo)
-            }
-        }
-    }
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+	for y, linha := range mapa {
+		for x, elem := range linha {
+			if efeitoNeblina == false || revelado[y][x] {
+				termbox.SetCell(x, y, elem.simbolo, elem.cor, elem.corFundo)
+			} else {
+				termbox.SetCell(x, y, neblina.simbolo, neblina.cor, neblina.corFundo)
+			}
+		}
+	}
 
-    desenhaBarraDeStatus()
+	desenhaBarraDeStatus()
 
-    termbox.Flush()
+	termbox.Flush()
 }
 
 func desenhaBarraDeStatus() {
-    for i, c := range statusMsg {
-        termbox.SetCell(i, len(mapa)+1, c, termbox.ColorBlack, termbox.ColorDefault)
-    }
-    msg := "Use WASD para mover e E para interagir. ESC para sair."
-    for i, c := range msg {
-        termbox.SetCell(i, len(mapa)+3, c, termbox.ColorBlack, termbox.ColorDefault)
-    }
+	for i, c := range statusMsg {
+		termbox.SetCell(i, len(mapa)+1, c, termbox.ColorBlack, termbox.ColorDefault)
+	}
+	msg := "Use WASD para mover e E para interagir. ESC para sair."
+	for i, c := range msg {
+		termbox.SetCell(i, len(mapa)+3, c, termbox.ColorBlack, termbox.ColorDefault)
+	}
 }
 
 func revelarArea() {
-    minX := max(0, posX-raioVisao)
-    maxX := min(len(mapa[0])-1, posX+raioVisao)
-    minY := max(0, posY-raioVisao/2)
-    maxY := min(len(mapa)-1, posY+raioVisao/2)
+	minX := max(0, posX-raioVisao)
+	maxX := min(len(mapa[0])-1, posX+raioVisao)
+	minY := max(0, posY-raioVisao/2)
+	maxY := min(len(mapa)-1, posY+raioVisao/2)
 
-    for y := minY; y <= maxY; y++ {
-        for x := minX; x <= maxX; x++ {
-            // Revela as células dentro do quadrado de visão
-            revelado[y][x] = true
-        }
-    }
+	for y := minY; y <= maxY; y++ {
+		for x := minX; x <= maxX; x++ {
+			// Revela as células dentro do quadrado de visão
+			revelado[y][x] = true
+		}
+	}
 }
 
 func max(a, b int) int {
-    if a > b {
-        return a
-    }
-    return b
+	if a > b {
+		return a
+	}
+	return b
 }
 
 func min(a, b int) int {
-    if a < b {
-        return a
-    }
-    return b
+	if a < b {
+		return a
+	}
+	return b
 }
 
 func mover(comando rune) {
-    dx, dy := 0, 0
-    switch comando {
-    case 'w':
-        dy = -1
-    case 'a':
-        dx = -1
-    case 's':
-        dy = 1
-    case 'd':
-        dx = 1
-    }
-    novaPosX, novaPosY := posX+dx, posY+dy
-    if novaPosY >= 0 && novaPosY < len(mapa) && novaPosX >= 0 && novaPosX < len(mapa[novaPosY]) &&
-        mapa[novaPosY][novaPosX].tangivel == false {
-        mapa[posY][posX] = ultimoElementoSobPersonagem // Restaura o elemento anterior
-        ultimoElementoSobPersonagem = mapa[novaPosY][novaPosX] // Atualiza o elemento sob o personagem
-        posX, posY = novaPosX, novaPosY // Move o personagem
-        mapa[posY][posX] = personagem // Coloca o personagem na nova posição
-    }
+	dx, dy := 0, 0
+	switch comando {
+	case 'w':
+		dy = -1
+	case 'a':
+		dx = -1
+	case 's':
+		dy = 1
+	case 'd':
+		dx = 1
+	}
+	novaPosX, novaPosY := posX+dx, posY+dy
+	if novaPosY >= 0 && novaPosY < len(mapa) && novaPosX >= 0 && novaPosX < len(mapa[novaPosY]) &&
+		mapa[novaPosY][novaPosX].tangivel == false {
+		mapa[posY][posX] = ultimoElementoSobPersonagem         // Restaura o elemento anterior
+		ultimoElementoSobPersonagem = mapa[novaPosY][novaPosX] // Atualiza o elemento sob o personagem
+		posX, posY = novaPosX, novaPosY                        // Move o personagem
+		mapa[posY][posX] = personagem                          // Coloca o personagem na nova posição
+	}
 }
 
 func interagir() {
-    //para cada celula na matriz num raio de 2 celulas, interage com o elemento mais próximo
-    for y := max(0, posY-2); y <= min(len(mapa)-1, posY+2); y++ {
-        for x := max(0, posX-2); x <= min(len(mapa[y])-1, posX+2); x++ {
-            if mapa[y][x].simbolo == chave.simbolo {
-                statusMsg = "Você pegou a chave!"
-                mapa[y][x] = vazio
-            } else if mapa[y][x].simbolo == porta.simbolo {
-                if statusMsg == "Você pegou a chave!" {
-                    statusMsg = "Você abriu a porta! Parabéns!"
-                    mapa[y][x] = vazio
-                    ganhei = true
-                } else {
-                    statusMsg = "Você precisa da chave!"
-                }
-            }
-        }
-    }
+	//para cada celula na matriz num raio de 2 celulas, interage com o elemento mais próximo
+	for y := max(0, posY-2); y <= min(len(mapa)-1, posY+2); y++ {
+		for x := max(0, posX-2); x <= min(len(mapa[y])-1, posX+2); x++ {
+			if mapa[y][x].simbolo == chave.simbolo {
+				statusMsg = "Você pegou a chave!"
+				mapa[y][x] = vazio
+			} else if mapa[y][x].simbolo == porta.simbolo {
+				if statusMsg == "Você pegou a chave!" {
+					statusMsg = "Você abriu a porta! Parabéns!"
+					mapa[y][x] = vazio
+					ganhei = true
+				} else {
+					statusMsg = "Você precisa da chave!"
+				}
+			}
+		}
+	}
 }
 
 func logicaInimigo() {
@@ -291,10 +306,10 @@ func logicaInimigo() {
 		curX, curY := i.x, i.y
 		speedX := rand.Intn(3) - 1 // Generate a random speed for X direction (-1, 0, 1)
 		speedY := rand.Intn(3) - 1 // Generate a random speed for Y direction (-1, 0, 1)
-		i.x += speedX          // Update i's X position
-		i.y += speedY          // Update i's Y position
+		i.x += speedX              // Update i's X position
+		i.y += speedY              // Update i's Y position
 		mutex.Lock()
-		mapa[curY][curX] = vazio     // Clear previous i position on the map
+		mapa[curY][curX] = vazio // Clear previous i position on the map
 		mapa[i.y][i.x] = inimigo // Update i's new position on the map
 		desenhaTudo()
 		mutex.Unlock()
